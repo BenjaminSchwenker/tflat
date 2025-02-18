0e1637ed11d101694b5f8cf5ccc0f5b83adb6d5f
benni <benjamin.schwenker@phys.uni-goettingen.de>
2025-02-18 13:16:06 +0100

    add clipping layer to mitigate outlier values

--- optimize.py
+++ optimize.py
@@ -23,10 +23,11 @@ def objective(trial):
     tf.random.set_seed(1234)
 
     parameters = {}
+    parameters["clip_value"] = 4
     parameters['mask_value'] = 0
     parameters['num_tracks'] = 10
     parameters['num_features'] = 14
-    parameters["num_transformer_blocks"] = 3
+    parameters["num_transformer_blocks"] = 6
     parameters["num_heads"] = 4
     parameters["embedding_dims"] = 128
     parameters["mlp_hidden_units_factors"] = [2, 1,]
